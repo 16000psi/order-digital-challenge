ea8160b6a351334a42dc560581cf1f63b6ac7597
16000psi <davidjhnsmith@gmail.com>
2024-04-01 19:35:29 +0100

    Add base frontend functionality to plainly serve data from API

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from "cors";
 import data from "./data/data.json" assert { type: "json" };
 
 const app = express();
 const port = 11194;
+
+app.use(cors());
 
 app.get("/api/data", (_, res) => {
   res.json(data);
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,49 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const url = new URL("http://localhost:11194/api/data");
+
+        const response = await fetch(url);
+        const jsonData = await response.json();
+        setData(jsonData);
+
+      } catch (error) {
+        throw new Error(`Error contacting API: ${response.statusText}`);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
+    <div>
+      <h2>Restaurant Portal</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rating</th>
+            <th>Cuisine</th>
+            <th>Distance (km)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, i) => (
+            <tr key={i}>
+              <td>{item.name}</td>
+              <td>{item.rating}</td>
+              <td>{item.cuisine}</td>
+              <td>{item.distance_km}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-export default App
+export default App;
